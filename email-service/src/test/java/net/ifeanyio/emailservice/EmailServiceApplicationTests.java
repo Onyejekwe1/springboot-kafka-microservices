a8ca68159d9166796d0269510cde866da0a728cd
Ifeanyi Onyejekwe <Ifeanyiwisdom25@yahoo.com>
2022-12-30 17:04:39 +0100

    Initial commit

---
+++ email-service/src/test/java/net/ifeanyio/emailservice/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ifeanyio.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
